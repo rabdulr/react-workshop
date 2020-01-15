85eee0e4001645fff3b9bb4010b013ab6f6b2c13
Redzuan <rabdulr@icloud.com>
2020-01-15 08:10:20 -0800

    States, Numbers

--- react-workshop/src/Stats.js
+++ react-workshop/src/Stats.js
@@ -3,20 +3,27 @@ import data from './data';
 
 const { users, companies, products } = data
 
-const states = users.reduce( (acc, user) => {
-    const key = user.state;
-    acc[key] = users.filter(state => state.state === key).length
-    return acc;
-}, {});
+const getState = (info) => {
 
-const state = Object.keys(states)
+    const states = info.reduce( (acc, item) => {
+        const key = item.state;
+        acc[key] = info.filter(state => state.state === key).length
+        return acc;
+    }, {});
+    
+    const state = Object.keys(states)
+    
+    const values = Math.max(...Object.values(states))
+    const valIndex = Object.values(states).indexOf(values)
+    const finalState = state[valIndex]
 
-const values = Math.max(...Object.values(states))
-const valIndex = Object.values(states).indexOf(values)
-const finalState = state[valIndex]
+    const final = [ finalState, values ]
 
+    return final;
 
+}
 
+console.log(products)
 
 function statsApp(){
     return(
@@ -24,13 +31,13 @@ function statsApp(){
             <h2>Stats</h2>
                 <ul>
                     <li>
-                    The most popular state for users is { finalState } with { values } users.
+                    The most popular state for users is { getState(users)[0] } with { getState(users)[1] } users.
                     </li>
                     <li>
-                    The most popular state for companies is {} with {} companies.
+                    The most popular state for companies is { getState(companies)[0] } with { getState(companies)[1]} companies.
                     </li>
                     <li>
-                    The most popular material for producs is {} with {} products.
+                    The most popular material for producs is { getState(products)[0]} with { getState(products)[1]} products.
                     </li>
                 </ul>
         </div>
